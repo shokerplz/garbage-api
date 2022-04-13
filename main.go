9131d98f22a7a4e220d0fb504f4d2b882952b34e
Ivan Kovalev <shoker444@gmail.com>
2022-04-13 19:49:17 +0300

    Log to DB

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+        "log"
 	"crypto/sha256"
 	"database/sql"
 	"encoding/hex"
@@ -24,6 +25,30 @@ type userStruct struct {
 func NewSHA256(data []byte) []byte {
 	hash := sha256.Sum256(data)
 	return hash[:]
+}
+
+func PrettyPrint(v interface{}) (ans string) {
+      b, err := json.MarshalIndent(v, "", "")
+      if err == nil {
+              return(string(b))
+      }
+      return ""
+}
+
+func logRequest(handler http.Handler, db *sql.DB) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		headers := ""
+		for k, v := range r.Header {
+			headers += k+":"+fmt.Sprint(v)+" "
+		}
+		sql := fmt.Sprintf("INSERT INTO logs(remote_addr, method, url, proto, headers, content_length, user_agent) VALUES ('%s', '%s', '%s', '%s', '%s', '%d', '%s')", r.RemoteAddr, r.Method, r.URL, r.Proto, headers, r.ContentLength, r.Header["User-Agent"])
+		_, err := db.Exec(sql)
+		if err != nil {
+			log.Print("Can't write logs to DB")
+		}
+		log.Printf(`{"remote_addr" : "%s", "method": "%s", "url": "%s", "proto" : "%s", "headers" : "%s", "content_length" : "%d", "user_agent" : "%s"}`, r.RemoteAddr, r.Method, r.URL, r.Proto, headers, r.ContentLength, r.Header["User-Agent"])
+		handler.ServeHTTP(w, r)
+	})
 }
 
 func register(w http.ResponseWriter, req *http.Request, db *sql.DB) {
@@ -87,14 +112,14 @@ func main() {
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(10)
 	sessionStorage = map[string]string{}
-	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/api/v1/register", func(w http.ResponseWriter, r *http.Request) {
 		register(w, r, db)
 	})
-	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/api/v1/login", func(w http.ResponseWriter, r *http.Request) {
 		login(w, r, db)
 	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
+		fmt.Fprintf(w, "Bookking")
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", logRequest(http.DefaultServeMux, db))
 }
